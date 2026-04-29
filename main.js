3a2f63d78cd2d2e288a3d3fdd3efe161d55b2e11
Szymon <gluon-siac.0t@icloud.com>
2026-04-29 19:13:19 +0200

    Konfiguracja połączenia z serwerem na Render.com

--- main.js
+++ main.js
@@ -73,9 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     /* Konfiguracja API */
-    const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://localhost:3000'
-        : 'https://link.onrender.com';
+    const API_URL = 'https://projekt-strona-72e7.onrender.com';
 
     const configForm = document.getElementById('configurator-form');
     if (configForm) {
